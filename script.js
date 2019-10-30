265e1d50c21dfed6c9b81eb20e89205d807a8d0c
Alexander Sardinas <21345@columbushs.com>
2019-10-30 10:29:27 -0400

    Fixed

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-    $('#orb').click( function() {        
+
+$(document).ready(function () {
+    $('#orb').click(function () {
         if ($('#orb').hasClass('sun')) {
             $('#orb').removeClass('sun').addClass('moon');
         }
         else {
             $('#orb').removeClass('moon').addClass('sun');
         }
-    
+
         if ($('#sky').hasClass('day')) {
             $('#sky').removeClass('day').addClass('night');
         }
@@ -18,5 +19,18 @@ $(document).ready(function() {
             $('#moonspot1').removeClass('visible');
         } else {
             $('#moonspot1').addClass('visible');
-        }
+        };
+        
+        if ($('#moonspot2').hasClass('visible')) {
+            $('#moonspot2').removeClass('visible');
+        } else {
+            $('#moonspot2').addClass('visible');
+        };
+
+        if ($('#moonspot3').hasClass('visible')) {
+            $('#moonspot3').removeClass('visible');
+        } else {
+            $('#moonspot3').addClass('visible');
+        };
+    });
 });
