cb17612f989599871a0bd24e9021cd18177c53e1
Alex-Sardinas <asardinas21345@columbushs.com>
2019-10-29 23:19:27 -0400

    ...

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#orb').click( function() {        
+        if ($('#orb').hasClass('sun')) {
+            $('#orb').removeClass('sun').addClass('moon');
+        }
+        else {
+            $('#orb').removeClass('moon').addClass('sun');
+        }
+    
+        if ($('#sky').hasClass('day')) {
+            $('#sky').removeClass('day').addClass('night');
+        }
+        else {
+            $('#sky').removeClass('night').addClass('day');
+        }
+
+        if ($('#moonspot1').hasClass('visible')) {
+            $('#moonspot1').removeClass('visible');
+        } else {
+            $('#moonspot1').addClass('visible');
+        }
+});
